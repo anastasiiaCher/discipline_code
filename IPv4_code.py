863597fb32e9e1821e54272f69d91a9044970997
anastasia_cher <anastasiia.chernysheva.2305@gmail.com>
2020-07-25 20:52:40 +0300

    Update IPv4_code.py

--- IPv4_code.py
+++ IPv4_code.py
@@ -38,17 +38,13 @@ ognp_module = {
     "13": "гиа"
 }
 ognp_codes = {
-    "1": ["12.03.04", "14.03.01", "16.03.03", "18.03.02", "19.03.01", "16.04.03", "18.04.02", "19.04.01", "19.04.02",
-          "19.04.03", "20.04.01", "27.04.01"],
-    "2": ["12.03.01", "13.03.02", "15.03.04", "15.03.06", "24.03.02", "27.03.04", "11.04.03", "12.04.01", "13.04.02",
-          "15.04.02", "15.04.04", "15.04.06", "23.04.03", "24.04.01", "24.04.02", "27.04.03"],
-    "3": ["09.03.01", "09.03.04", "10.03.01", "11.03.03", "23.03.03", "44.03.04", "09.04.01", "09.04.04", "10.04.01",
-          "27.04.03", "27.04.04"],
+    "1": ["12.03.04", "14.03.01", "16.03.03", "18.03.02", "19.03.01", "16.04.03", "18.04.02", "19.04.01", "19.04.02", "19.04.03", "20.04.01", "27.04.01"],
+    "2": ["12.03.01", "13.03.02", "15.03.04", "15.03.06", "24.03.02", "27.03.04", "11.04.03", "12.04.01", "13.04.02", "15.04.02", "15.04.04", "15.04.06", "23.04.03", "24.04.01", "24.04.02", "27.04.03"],
+    "3": ["09.03.01", "09.03.04", "10.03.01", "11.03.03", "23.03.03", "44.03.04", "09.04.01", "09.04.04", "10.04.01", "27.04.03", "27.04.04"],
     "4": ["27.03.05", "38.03.05", "27.04.02", "27.04.05", "27.04.08", "38.04.01", "38.04.05"],
     "5": ["01.03.02", "09.03.02", "01.04.02", "02.04.03", "09.04.02"],
     "6": ["09.03.03", "11.03.02", "45.03.04", "07.04.04", "09.04.03", "11.04.02", "27.04.07", "45.04.04"],
-    "7": ["12.03.02", "12.03.03", "12.03.05", "12.05.01", "16.03.01", "12.04.02", "12.04.03", "12.04.04", "12.04.05",
-          "16.04.01"]
+    "7": ["12.03.02", "12.03.03", "12.03.05", "12.05.01", "16.03.01", "12.04.02", "12.04.03", "12.04.04", "12.04.05", "16.04.01"]
 }
 
 
@@ -61,54 +57,50 @@ def clean_text(text):
     return cleaned
 
 
-# calculate first position
-def get_pos_1(xlsx_degree):
-    for d in degree:
-        if re.search(degree[d], xlsx_degree, flags=re.IGNORECASE): return d
-
-
-# calculate second and third positions
-def get_pos_2_3(p_1, xlsx_subfieldcode, xlsx_component, xlsx_subj_code):
-    for u in uni_module:
-        if re.search(uni_module[u], xlsx_component, flags=re.IGNORECASE):
-            if u != "11":
-                return "0", u
-            elif ((p_1 in ["06", "071"]) and xlsx_subj_code <= 9) or (p_1 == "072" and xlsx_subj_code <= 2):
-                return "0", u
-    for p in ognp_module:
-        if re.search(ognp_module[p], xlsx_component, flags=re.IGNORECASE):
-            for num in ognp_codes:
-                if xlsx_subfieldcode in ognp_codes[num]:
-                    return num, p
-        elif re.match("модуль [0-9]", xlsx_component, flags=re.IGNORECASE):
-            for num in ognp_codes:
-                if xlsx_subfieldcode in ognp_codes[num]:
-                    return num, "5"
+# calculate positions 1-3
+def get_pos_1_2_3(xlsx_degree, xlsx_sf_code, xlsx_comp, xlsx_subj_code, line=""):
+    list_of_degrees = [d for d in degree if re.search(degree[d], xlsx_degree, flags=re.IGNORECASE)]
+    if not list_of_degrees: sys.exit("Неизвестный уровень образования в %sзаписи." % line)
+    else: p1 = list_of_degrees[0]
+    uni = [u for u in uni_module if re.match(uni_module[u], xlsx_comp, flags=re.IGNORECASE)]
+    module = ["5" if re.match("модуль [0-9]", xlsx_comp, flags=re.IGNORECASE) else
+              p for p in ognp_module
+              if re.match(ognp_module[p], xlsx_comp, flags=re.IGNORECASE)]
+    if not uni and not module: sys.exit("Неизвестный модуль в %sзаписи." % line)
+    if uni:
+        p_1 = p1
+        if p1 == "071": p_1 = "06"
+        if uni[0] != "11": return p_1 + "." + "0" + "." + uni[0] + "."
+        elif (p_1 == "06" and xlsx_subj_code <= 9) or (p_1 == "072" and xlsx_subj_code <= 2): return p_1 + "." + "0" + "." + uni[0] + "."
+    if module:
+        ognp_num = [num for num in ognp_codes if xlsx_sf_code in ognp_codes[num]]
+        if not ognp_num: sys.exit("Неизвестный шифр направления подготовки в %sзаписи." % line)
+        else: return p1 + "." + ognp_num[0] + "." + module[0] + "."
 
 
 # find max 4th value
 def get_max_4(dis_rep):
-    list_of_4 = [int(dis_rep["DIS_CODE"].values[d].split("(", 1)[0].split(".")[3]) for d in
-                 range(0, len(dis_rep["DIS_CODE"]))]
+    list_of_4 = [int(dis_rep["DIS_CODE"][d].split(".")[3]) for d in dis_rep.index.values]
     if list_of_4: return max(list_of_4)
     else: return -1
 
 
-def extract_3(dis_code):
-    return dis_code.split("(", 1)[0].split(".")[2]
+# collect info on semesters and credits
+def unit_info(data, sf_name, subj, comp, subj_code):
+    credit_units = [0 for i in range(0, 12)]
+    units = data.loc[(data["SUBFIELDNAME"] == sf_name) & (data["SUBJECT"] == subj) & (data["COMPONENT"] == comp) & (data["SUBJECT_CODE"] == subj_code)]
+    try:
+        for u in units.index.values:
+            if pd.isna(units["CREDITS"][u]): credit_units[int(units["SEMESTER"][u]) - 1] = "-"
+            elif units["SEMESTER"][u] == ".": credit_units[11] = int(units["CREDITS"][u])
+            else: credit_units[int(units["SEMESTER"][u]) - 1] = int(units["CREDITS"][u])
+    except:
+        pass
+    return ",".join(map(str, credit_units))
 
 
-def sem_info(data_frame, subfield_name, subj, component, subj_code):
-    df_1 = data_frame.loc[(data_frame["SUBFIELDNAME"] == subfield_name)
-                          & (data_frame["SUBJECT"] == subj)
-                          & (data_frame["COMPONENT"] == component)
-                          & (data_frame["SUBJECT_CODE"] == subj_code)]
-    sem_list = df_1["SEMESTER"].to_list()
-    cred_list = df_1["CREDITS"].to_list()
-    return "".join(map(str, sem_list)) + "/" + "".join(str(c) for c in cred_list).replace(".0", "")
-
-
-def get_pos_4(rep, sem_xlsx, dis_code, subj, subfield_name):
+# calculate position 4
+def get_pos_4(rep, sem_xlsx, dis_code, subj, sf_name):
     if subj not in rep["SUBJECT"].to_list(): return str(get_max_4(rep) + 1)
     else:
         rep1 = rep.loc[rep["SUBJECT"] == subj]
@@ -117,56 +109,54 @@ def get_pos_4(rep, sem_xlsx, dis_code, subj, subfield_name):
             rep2 = rep1.loc[rep1["DIS_CODE"].str.match(dis_code)]
             if sem_xlsx not in rep2["SEM_INFO"].to_list(): return str(get_max_4(rep) + 1)
             else:
-                rep3 = rep2.loc[rep2["SEM_INFO"] == sem_xlsx]
+                rep3 = rep2.loc[rep2["SEM_INFO"] == sem_xlsx].reset_index(drop=True)
                 rows = len(rep3["DIS_CODE"])
                 count = 0
-                for p in range(0, len(rep3["DIS_CODE"])):
-                    if (rep3["SUBFIELDNAME"].values[p] != subfield_name) and (extract_3(dis_code) in ["12", "13"]):
+                for p in rep3.index.values:
+                    if (rep3["SUBFIELDNAME"][p] != sf_name) and (dis_code.split(".")[2] in ["12", "13"]):
                         count += 1
                         if count != rows: continue
                         else: return str(get_max_4(rep) + 1)
-                    else: return str(rep3["DIS_CODE"].values[p].split(".")[3])
+                    else: return str(rep3["DIS_CODE"][p].split(".")[3])
 
 
-# generate unique code for each discipline
+# create sys_df if empty or does not exist
+def create_sys_df():
+    cols = ["SUBFIELDCODE", "MAJOR_NAME", "SUBFIELDNAME", "YEAR", "DEGREE", "SUBJECT_CODE", "SUBJECT", "CREDITS", "CYCLE", "COMPONENT", "SEMESTER", "ISOPTION", "TYPELEARNING", "DIS_CODE", "SEM_INFO"]
+    sys_df = pd.DataFrame(columns=cols)
+    return sys_df
+
+
+# generate unique code for each discipline in excel file
 def generate_df_w_unique_code(in_df, sys_df=None):
     code_list = []
     out_df = in_df.copy()
-    if (sys_df is None) or sys_df.empty:
-        cols = list(in_df)
-        cols.append("DIS_CODE")
-        cols.append("SEM_INFO")
-        sys_df = pd.DataFrame(columns=cols)
+    if (sys_df is None) or sys_df.empty: sys_df = create_sys_df()
     in_df["COMPONENT"] = in_df["COMPONENT"].apply(clean_text)
-    sys_df["COMPONENT"] = sys_df["COMPONENT"].apply(clean_text)
-    for i in range(0, len(in_df["SUBFIELDNAME"])):
-        sem = sem_info(in_df,
-                       in_df.loc[i, "SUBFIELDNAME"],
-                       in_df.loc[i, "SUBJECT"],
-                       in_df.loc[i, "COMPONENT"],
-                       in_df.loc[i, "SUBJECT_CODE"])
-        pos_1 = get_pos_1(in_df.loc[i, "DEGREE"])
-        if pos_1 is None:
-            print("В строке", i + 2, "содержится название неизвестного уровня образования.")
-            sys.exit()
-        try:
-            pos_2, pos_3 = get_pos_2_3(pos_1,
-                                       in_df.loc[i, "SUBFIELDCODE"],
-                                       in_df.loc[i, "COMPONENT"],
-                                       in_df.loc[i, "SUBJECT_CODE"])
-        except:
-            print("В строке", i + 2, "содержится название неизвестного модуля или неизвестный шифр направления подготовки.")
-            sys.exit()
-        in_df.loc[i, "DIS_CODE"] = pos_1 + "." + pos_2 + "." + pos_3 + "."
-        pos_4 = get_pos_4(sys_df,
-                          sem,
-                          in_df.loc[i, "DIS_CODE"],
-                          in_df.loc[i, "SUBJECT"],
-                          in_df.loc[i, "SUBFIELDNAME"])
-        in_df.loc[i, "DIS_CODE"] = in_df.loc[i, "DIS_CODE"] + str(pos_4) + "." + str(in_df.loc[i, "YEAR"])
+    for i in in_df.index.values:
+        sem = unit_info(in_df, in_df["SUBFIELDNAME"][i], in_df["SUBJECT"][i], in_df["COMPONENT"][i], in_df["SUBJECT_CODE"][i])
+        in_df.loc[i, "DIS_CODE"] = get_pos_1_2_3(in_df["DEGREE"][i], in_df["SUBFIELDCODE"][i], in_df["COMPONENT"][i], in_df["SUBJECT_CODE"][i], str(i + 2) + " ")
+        p4 = get_pos_4(sys_df, sem, in_df["DIS_CODE"][i], in_df["SUBJECT"][i], in_df["SUBFIELDNAME"][i])
+        in_df.loc[i, "DIS_CODE"] = in_df["DIS_CODE"][i] + str(p4) + "." + str(in_df.loc[i, "YEAR"])
+        print(in_df.loc[i, "DIS_CODE"])
         sys_df = sys_df.append(in_df.loc[i])
         sys_df.iloc[-1, sys_df.columns.get_loc("SEM_INFO")] = sem
-        sys_df = sys_df.drop_duplicates()
-        code_list.append(in_df.loc[i, "DIS_CODE"])
+        sys_df = sys_df.drop_duplicates().reset_index(drop=True)
+        code_list.append(in_df["DIS_CODE"][i])
     out_df["DIS_CODE"] = code_list
     return out_df, sys_df
+
+
+# generate unique code for a discipline that already exists
+def generate_single_unique_code(comp, subj_degree, sf_code, subj_code, subj, sf_name, credit_units, year, sys_df=None):
+    if (sys_df is None) or sys_df.empty: sys_df = create_sys_df()
+    comp = clean_text(comp)
+    dis_code = get_pos_1_2_3(subj_degree, sf_code, comp, subj_code)
+    sem = ",".join(map(str, credit_units))
+    p4 = get_pos_4(sys_df, sem, dis_code, subj, sf_name)
+    dis_code = dis_code + str(p4) + "." + year
+    to_append = [sf_code, "", sf_name, year, subj_degree, subj_code, subj, "", "", comp, "", "", "", dis_code, sem]
+    new_row = pd.Series(to_append, index=sys_df.columns)
+    sys_df = sys_df.append(new_row, ignore_index=True)
+    sys_df = sys_df.drop_duplicates()
+    return dis_code, sys_df
